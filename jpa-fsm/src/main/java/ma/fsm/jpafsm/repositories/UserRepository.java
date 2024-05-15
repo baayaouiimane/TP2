459a2d997fd85c48a81c8a30bc864912625f3a6e
baayaouiimane <167249908+baayaouiimane@users.noreply.github.com>
2024-05-15 11:24:50 +0100

    Delete jpa-fsm directory

--- jpa-fsm/src/main/java/ma/fsm/jpafsm/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package ma.fsm.jpafsm.repositories;
-
-import ma.fsm.jpafsm.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,String> {
-    User findByUsername(String userName);
-}
